b62b6ebe4d51a19b6ca30146438fe785d39f3846
PTYZT604 <PTYZT604@users.noreply.github.com>
2023-03-18 14:44:48 +0800

    Unit4: Resize the Width and height of btns&bk.   btns:     Width = 352     175-30     Height = 269    123
      bk:
        Left = 358      179
        Top = 229       103
    
    [Squashed] U4: Resize btn width and height,reset left and top (2023-03-18)

--- Unit4.pas
+++ Unit4.pas
@@ -53,10 +53,10 @@ begin
       begin
         tm[i,j]:=timage.Create(nil);
         tm[i,j].parent:=self;
-        tm[i,j].Height:=268;
-        tm[i,j].Width:=352;
-        tm[i,j].Left:=358+352*(i-1);
-        tm[i,j].Top:=229+268*(j-1);
+        tm[i,j].Height:=115;
+        tm[i,j].Width:=145;
+        tm[i,j].Left:=179+175*(i-1)+15;
+        tm[i,j].Top:=103+123*(j-1)+4;
         // if i mod 2=0 then tm[i,j].Top:=tm[i,j].Top+60;
         tm[i,j].Stretch:=true;
         tm[i,j].Visible:=true;
